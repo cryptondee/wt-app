bc45f8f935d12b90859b2ebdcaf7011dfb5e3d42
NDW <cryptondee@gmail.com>
2023-03-01 22:52:20 +0100

    changed h1

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
         <div className="max-w-md w-full space-y-8">
           <div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-              My Ethereum Token Tracker
+              Wallets Token Tracker
             </h2>
           </div>
           <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
